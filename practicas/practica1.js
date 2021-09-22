de2c0ce99541adf39f5fcdaedbe64a96617b3374
RosangelaV21 <valerorosangela@gmail.com>
2021-09-21 19:11:40 -0500

    primer commit

---
+++ practicas/practica1.js
@@ -0,0 +1,143 @@
+const departamento=()=>{
+
+    let nombredepa= prompt("Ingresa departamento");
+    
+    switch (nombredepa) {
+        case "Amazonas":
+            alert("La capital es Leticia");
+            break;
+    
+            case"Antioquía":
+            alert(" La capital esMedellín");
+              break;
+    
+            case "Arauca":
+                alert("La capital es Arauca");
+                break;
+    
+            case"Atlántico":
+            alert("La capital es Barranquilla");
+            break;
+            
+            case"Bolívar":
+            alert(" La capital es Cartagena de Indias");
+            break;
+    
+            case"Boyacá":
+            alert(" La capital es Tunja");
+            break;
+    
+            case"Caldas":
+            alert("La capital es Manizales");
+            break;
+            
+            case"Caquetá":
+            alert("La capital es Florencia");
+            break;
+    
+            case"Casanare":
+            alert("La capital es Yopal");
+            break;
+    
+            case"Cauca":
+            alert("La capital es Popayán");
+            break;
+    
+            case"Cesar":
+            alert(" La capital es Valledupar");
+            break;
+    
+            case"Chocó":
+            alert(" La capital es Quibdó");
+            break;
+    
+            case"Córdoba":
+            alert("Montería");
+            break;
+    
+            case"Cundinamarca":
+            alert("Bogotá");
+            break;
+    
+    
+            case"Guainía":
+            alert("Inírida");
+            break;
+    
+    
+            case"Guaviare":
+            alert("San José del Guaviare");
+            break;
+    
+            case"Huila":
+            alert("Neiva");
+            break;
+    
+            case"La Guajira":
+            alert("Riohacha");
+            break;
+    
+            case"Magdalena":
+            alert("Santa Marta");
+            break;
+    
+            case"Meta":
+            alert("Villavicencio");
+            break;
+    
+            case"Nariño":
+            alert("San Juan de Pasto");
+            break;
+    
+            case"Norte de santander":
+            alert("San José de Cúcuta");
+            break;
+    
+            case"Putumayo":
+            alert("Mocoa");
+            break;
+    
+            case"Quindío":
+            alert(" Armenia");
+            break;
+    
+            case"Risaralda":
+            alert("Pereira");
+            break;
+    
+            case"San Andrés y Providencia":
+            alert("San Andrés");
+            break;
+    
+            case"Santander":
+            alert("Bucaramanga");
+            break;
+    
+            case"Sucre":
+            alert("Sincelejo");
+            break;
+    
+            case"Tolima":
+            alert("Ibagué");
+            break;
+    
+            case"Valle del Cauca":
+            alert("Cali");
+            break;
+           
+            case"Vaupés":
+            alert("Mitú");
+            break;
+    
+            case"Vichada":
+            alert("Puerto Carreño");
+            break;
+    
+            
+        default:
+            alert("Inteta de nuevo (Colocas los departamento como aparecen en la lista");
+            break;
+    }
+    
+    
+    }
